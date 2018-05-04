05bb406b67a4e759cae33c5bd61e8a53038f9484
USP <USP@C202-10>
2018-05-03 23:02:16 -0500

    resgistro de notas y kardex

--- registro de notas/registro de notas/kardex de notas.aspx.designer.vb
+++ registro de notas/registro de notas/kardex de notas.aspx.designer.vb
@@ -93,4 +93,13 @@ Partial Public Class kardex_de_notas
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents GridView1 As Global.System.Web.UI.WebControls.GridView
+    
+    '''<summary>
+    '''Control Button1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
 End Class
